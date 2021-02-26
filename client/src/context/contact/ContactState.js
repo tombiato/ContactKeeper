bcde45f40397f9868b6e0fe388ac7a1a7e4d6106
Tom Biato <tom.biato@ynov.com>
2021-02-26 11:51:08 +0100

    Update Contacts

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -62,6 +62,26 @@ const ContactState = props => {
 		}
 	};
 
+	// Update contact
+	const updateContact = async contact => {
+		const config = {
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		};
+
+		try {
+			const res = await axios.put(
+				`/api/contacts/${contact._id}`,
+				contact,
+				config
+			);
+			dispatch({ type: UPDATE_CONTACT, payload: res.data });
+		} catch (err) {
+			dispatch({ type: CONTACT_ERROR, payload: err.response.msg });
+		}
+	};
+
 	// Clear contacts
 	const clearContacts = () => {
 		dispatch({ type: CLEAR_CONTACTS });
@@ -75,11 +95,6 @@ const ContactState = props => {
 	// Clear current contact
 	const clearCurrent = () => {
 		dispatch({ type: CLEAR_CURRENT });
-	};
-
-	// Update contact
-	const updateContact = contact => {
-		dispatch({ type: UPDATE_CONTACT, payload: contact });
 	};
 
 	// Filter contacts
